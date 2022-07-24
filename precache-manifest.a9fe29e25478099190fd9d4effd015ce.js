e954bfd3ad2df20bcdab119297ad5c3fc0831bbf
StarSheriff2 <StarSheriff2@users.noreply.github.com>
2022-07-24 21:41:42 +0000

    deploy: 7c68b467692dd7ab197fe97bc9e1e6854fa29e2c

---
+++ precache-manifest.a9fe29e25478099190fd9d4effd015ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bdbb3541351427f52ff9ea7f48cdb29",
+    "url": "https://StarSheriff2.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "efb24740d8a3b08dc6c7",
+    "url": "https://StarSheriff2.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "c1513ffc03be9aa5b08b",
+    "url": "https://StarSheriff2.github.io/task-management-frontend/static/js/2.d24dd898.chunk.js"
+  },
+  {
+    "revision": "efb24740d8a3b08dc6c7",
+    "url": "https://StarSheriff2.github.io/task-management-frontend/static/js/main.af8ad2bb.chunk.js"
+  },
+  {
+    "revision": "5e5c91f035d9a5b27a71",
+    "url": "https://StarSheriff2.github.io/task-management-frontend/static/js/runtime~main.bb018b0e.js"
+  }
+]);
